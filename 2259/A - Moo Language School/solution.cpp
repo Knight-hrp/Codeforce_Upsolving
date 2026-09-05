92569e0c1550ecacf83012ab97a9000c6cd7e3a5
Rishabh Patidar <patidarrishabh20@gmail.com>
2026-09-05 20:47:02 +0530

    Add Moo Language School [A] from Codeforces

---
+++ 2259/A - Moo Language School/solution.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        int ans = 0;
+        for(int i = 0; i < n; i= i + k)
+        {
+            int count = 0;
+            for(int j = i; j < i + k && j < n; j++)
+            {
+                if(s[j] == '1')
+                {
+                    count++;
+                }
+            }
+            if(count  == k)
+            {
+                ans++;
+            }
+        }
+ 
+        cout << ans << endl;
+ 
+    }
+    return 0;
+}
+ 
